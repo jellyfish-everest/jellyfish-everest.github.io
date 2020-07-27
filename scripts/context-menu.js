f414f6341988b410ed158bdbeb06e941fe237d18
John Zhang <zlgbox@hotmail.com>
2020-07-26 22:38:40 -0700

    Trying touch event for context menu for cleaner handling

--- scripts/context-menu.js
+++ scripts/context-menu.js
@@ -6,14 +6,23 @@
             // return native menu if pressing control
             if (e.ctrlKey) return;
 
+            let x = e.clientX;
+            let y = e.clientY;
+
+            if (e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel') {
+                var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+                x = touch.pageX;
+                y = touch.pageY;
+            }
+
             //open menu
             var $menu = $(settings.menuSelector)
                 .data("invokedOn", $(e.target))
                 .show()
                 .css({
                     position: "absolute",
-                    left: getMenuPosition(e.clientX, 'width', 'scrollLeft'),
-                    top: getMenuPosition(e.clientY, 'height', 'scrollTop')
+                    left: getMenuPosition(x, 'width', 'scrollLeft'),
+                    top: getMenuPosition(y, 'height', 'scrollTop')
                 })
                 .off('click')
                 .on('click', 'a', function (e) {
@@ -34,14 +43,33 @@
                 return _openConextMenu(e, settings);
             });
 
-            $(this).longclick(1000, function (e) {
-                return _openConextMenu(e, settings);
+            // Handle long press as well
+            // $(this).on("mousedown", function (e) {
+            //     $(this).data("checkdown", setTimeout(function () {
+            //         return _openConextMenu(e, settings);
+            //     }, 1000));
+            // }).on("mouseup", function () {
+            //     clearTimeout($(this).data("checkdown"));
+            // }).on("mouseout", function () {
+            //     clearTimeout($(this).data("checkdown"));
+            // });
+
+            $(this).on('touchend', function (e) {
+                clearTimeout($(this).data("pressTimer"));
+            }).on('touchstart', function (e) {
+                // Set timeout
+                $(this).data("pressTimer", setTimeout(function () {
+                    return _openConextMenu(e, settings);
+                }, 800));
             });
+
+            // $(this).longclick(1000, function (e) {
+            //     return _openConextMenu(e, settings);
+            // });
 
             // Hide when click other places
             $(window).click(function () {
                 $(settings.menuSelector).hide();
-                console.log("hide context")
             });
         });
 
